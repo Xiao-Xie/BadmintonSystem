6b2978f69f9ab719cf3370328d0f8f5ff30a9ceb
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-08-15 16:47:05 -0400

    update server index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-var db = require('../db');
 
 const app = express();
 
@@ -9,9 +8,13 @@ app.use(express.static(path.join(__dirname, '../client')));
 const PORT = process.env.PORT || 9000;
 
 //To prevent server routing - react-router will handle the routing
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+
+//routing
+const router = require('./router');
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
