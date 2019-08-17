54a7a8cf5b8a3bdec62f42f2def4075987a97b8a
Xiao Xie <xiexiaoxiexiao2009@gmail.com>
2019-08-17 15:07:24 -0400

    update seting

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: process.env.DB_HOST || 'us-cdbr-iron-east-02.cleardb.net',
-  user: process.env.DB_USER || 'bb573bda03820b',
-  password: process.env.DB_PASSWORD || '74a0061c',
-  database: process.env.DB_DATABASE || 'heroku_3524472e2e03a21',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '312312',
+  database: process.env.DB_DATABASE || 'newbee',
 };
